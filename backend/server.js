445cc9acbb75af12cb31e53f64f2ba6b474e45dd
Peter Huynh <peterhyh@yahoo.com>
2024-07-17 14:40:51 -0500

    SQL connected

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,28 @@ const express = require("express");
 const cors = require("cors")
 const mysql = require("mysql")
 
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "sports_stats"
+})
 
 app.get('/', (req, res) => {
     return res.json("From backend side");
 })
 
-app.listen(8081, () => {
-    console.log("Listening...");
+app.get('/history', (req, res) => {
+    const sql = 'SELECT * FROM history';
+    db.query(sql, (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.listen(8082, () => {
+    console.log("Listening");
 })
